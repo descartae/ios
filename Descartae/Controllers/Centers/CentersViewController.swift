1cab794868fe53f8e5cc2ecf789125af02ee5f39
filipealva <ofilipealvarenga@gmail.com>
2017-11-20 22:34:21 -0200

    Adjust CentersViewControllers to work based on the Center fragment

--- Descartae/Controllers/Centers/CentersViewController.swift
+++ Descartae/Controllers/Centers/CentersViewController.swift
@@ -10,9 +10,24 @@ import UIKit
 
 final class CentersViewController: UIViewController {
 
+    var centers: [Center] = []
+
     override func viewDidLoad() {
         super.viewDidLoad()
 
+        let allCentersQuery = AllCentersQuery()
+        GraphQL.client.fetch(query: allCentersQuery) { (result, error) in
+            guard error == nil else {
+                print(error?.localizedDescription ?? "")
+                return
+            }
+
+            guard let centersQueryFragment = result?.data?.centers as? [AllCentersQuery.Data.Center] else {
+                return
+            }
+
+            self.centers = Center.collection(fromQueryResult: centersQueryFragment)
+        }
     }
 
 }
