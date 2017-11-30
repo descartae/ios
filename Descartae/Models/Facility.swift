dfd0c4e6da8e9e896306668e86f7c78ec62fea2a
filipealva <ofilipealvarenga@gmail.com>
2017-11-30 11:54:08 -0200

    Add a computed properties on Facility.Location extension that compiles the facility's coordinates into a CLLocation object when they are available

--- Descartae/Models/Facility.swift
+++ Descartae/Models/Facility.swift
@@ -8,11 +8,24 @@
 
 import Foundation
 import Apollo
+import CoreLocation
 
 extension Facility: Equatable {
-    
+
     public static func == (lhs: Facility, rhs: Facility) -> Bool {
         return lhs.id == rhs.id
+    }
+
+}
+
+extension Facility.Location {
+
+    var location: CLLocation? {
+        guard let latitude = coordinates?.latitude, let longitude = coordinates?.longitude else {
+            return nil
+        }
+
+        return CLLocation(latitude: latitude, longitude: longitude)
     }
 
 }
