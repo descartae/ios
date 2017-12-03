3764c55ee352879226f715df01b8fb5a342e9bea
filipealva <ofilipealvarenga@gmail.com>
2017-12-03 20:02:48 -0200

    Rename it to Today instead of Top

---
+++ Descartae/Views/Facilities/Cells/OpenHoursTopTableViewCell.swift
@@ -0,0 +1,12 @@
+//
+//  OpenHoursTodayTableViewCell.swift
+//  Descartae
+//
+//  Created by Filipe Alvarenga on 03/12/17.
+//  Copyright © 2017 Filipe Alvarenga. All rights reserved.
+//
+
+import UIKit
+
+class OpenHoursTodayTableViewCell: UITableViewCell {
+}
