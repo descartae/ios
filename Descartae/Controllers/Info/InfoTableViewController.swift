0692180d0b0ce387977e9da3c7dd25103dd35a74
filipealva <ofilipealvarenga@gmail.com>
2018-01-17 00:47:49 -0200

    Implement about and send feedback

--- Descartae/Controllers/Info/InfoTableViewController.swift
+++ Descartae/Controllers/Info/InfoTableViewController.swift
@@ -8,8 +8,13 @@
 
 import UIKit
 import StoreKit
+import SafariServices
 
 class InfoTableViewController: UITableViewController {
+
+    // MARK: Properties
+
+    let aboutPage = "https://descartae.com/sobre"
 
     // MARK: Life cycle
 
@@ -28,15 +33,43 @@ class InfoTableViewController: UITableViewController {
         presentingViewController?.dismiss(animated: true, completion: nil)
     }
 
+    func goToAboutPage() {
+        guard let url = URL(string: aboutPage) else {
+            return
+        }
+
+        let safari = SFSafariViewController(url: url)
+        safari.modalPresentationStyle = .popover
+        safari.preferredControlTintColor = tableView.tintColor
+
+        present(safari, animated: true, completion: nil)
+    }
+
+    func showReviewController() {
+        SKStoreReviewController.requestReview()
+    }
+
+    // MARK: Navigation
+
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if let reportAnIssueNav = segue.destination as? ReportAnIssueNavigationController,
+                let reportAnIssue = reportAnIssueNav.childViewControllers[0] as? ReportAnIssueTableViewController {
+            reportAnIssue.isGeneralAppFeedback = true
+        }
+    }
+
 }
 
 extension InfoTableViewController {
 
     override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         switch indexPath.row {
+        case 1:
+            tableView.deselectRow(at: indexPath, animated: true)
+            goToAboutPage()
         case 3:
             tableView.deselectRow(at: indexPath, animated: true)
-            SKStoreReviewController.requestReview()
+            showReviewController()
         default:
             break
         }
